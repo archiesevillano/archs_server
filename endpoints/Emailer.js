3eddbd50b24bebc25c3cd846d2b168b70819da1c
archiesevillano <archie.sevillano29@gmail.com>
2023-07-05 15:08:06 +0800

    Email Function Added

--- endpoints/Emailer.js
+++ endpoints/Emailer.js
@@ -1,32 +1,41 @@
 const express = require("express");
 const router = express.Router();
 const nodemailer = require("nodemailer");
+const { MAIL_ACC, CLIENT_ID, REFRESH_TOKEN, ACCESS_TOKEN, CLIENT_SECRET } = process.env;
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    port: 465,
-    secure: true,
+    service: 'gmail',
     auth: {
-        // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-        user: 'archie.sevillano29@gmail.com', //REPLACE-WITH-YOUR-ALIAS@YOURDOMAIN.COM
-        pass: '136782070733' //REPLACE-WITH-YOUR-GENERATED-PASSWORD
+        user: MAIL_ACC,
+        pass: 'mpchpzbcreeelgbr'
     },
-    tls: { rejectUnauthorized: false }
+    tls: {
+        rejectUnauthorized: false
+    }
 });
+
+const send = async () => {
+    const result = await transporter.sendMail({
+        from: MAIL_ACC,
+        to: "archie.sevillano29@gmail.com",
+        subject: 'Hello World',
+        html: '<h1 style="background: red; color: white">This is a test</h1>'
+    });
+
+    const info = await JSON.stringify(result, null, 4);
+    console.log(info);
+    return info;
+}
 
 router.post("/", async (req, res) => {
     try {
+        //data to send
+        const { receiver, message, subject } = req.body;
 
-        // send mail with defined transport object
-        const info = await transporter.sendMail({
-            from: 'archie.sevillano29@gmail.com', // sender address
-            to: "aczionacs01@gmail.com", // list of receivers
-            subject: "Test Email from Archs Portfolio", // Subject line
-            html: "<b>This is a test email</b>", // html body
-        });
-
-        //send response result back to client
-        res.send(info);
+        console.log([receiver, message, subject]);
+        //start sending email
+        const data = await send();
+        res.send(data);
     }
     catch (err) {
         res.send(err);
@@ -35,5 +44,3 @@ router.post("/", async (req, res) => {
 });
 
 module.exports = router;
-
-//https://stackoverflow.com/questions/72470777/nodemailer-response-535-5-7-8-username-and-password-not-accepted
