35688ec259b73c3bbbeddf21fc403e08e1cfcd4e
archiesevillano <archie.sevillano29@gmail.com>
2023-08-18 16:12:40 +0800

    Deleted console logs and fixed conflict between /download routes

--- endpoints/File.js
+++ endpoints/File.js
@@ -3,7 +3,30 @@ const router = express.Router();
 const { app } = require("../firebase");
 const { getStorage, ref, getDownloadURL } = require("firebase/storage");
 const axios = require("axios");
-const basePath = "Technologies";
+
+// Alternative Download Link for CV
+router.get("/download/alternative-download-cv", async (req, res) => {
+    try {
+
+        //get fileid from GoogleDrive File Share Link between 'd' and 'view' in url e.g (https://drive.google.com/file/d/[FILE ID]/view?usp=sharing)
+        //paste File ID next to "id=" in your final link which is "https://drive.google.com/uc?export=download&id="
+        const gDriveFile = "https://drive.google.com/uc?export=download&id=1tLDU0Qc8gePDphKUZkJTCfhv6_33yRma";
+
+        const response = await axios({
+            url: gDriveFile,
+            method: "GET",
+            responseType: "stream",
+        });
+
+        response.data.pipe(res);
+
+    }
+    catch (error) {
+        res.send(error);
+        console.log(error);
+    }
+
+});
 
 // Direct Download Link for CV
 router.get("/download/:filename", async (req, res) => {
@@ -24,22 +47,7 @@ router.get("/download/:filename", async (req, res) => {
     }
     catch (error) {
         res.send(error);
-    }
-
-});
-
-// Alternative Download Link for CV
-router.get("/download/alternative-download-cv", async (req, res) => {
-    try {
-
-        //get fileid from GoogleDrive File Share Link between 'd' and 'view' in url e.g (https://drive.google.com/file/d/[FILE ID]/view?usp=sharing)
-        //paste File ID next to "id=" in your final link which is "https://drive.google.com/uc?export=download&id="
-        const gDriveFile = "https://drive.google.com/uc?export=download&id=1ZH_9jgC38AQ1YqHqHMkx9HeDzdilgtUw";
-
-        res.send(gDriveFile);
-    }
-    catch (error) {
-        res.send(error);
+        console.log(error);
     }
 
 });
